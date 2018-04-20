071d8cf6fdb1a132ca7d4ca639669b6f1029661c
jritch <jacob.w.k.ritchie@gmail.com>
2018-04-19 21:20:45 -0400

    added real data

--- app.py
+++ app.py
@@ -23,15 +23,18 @@ def db_query(fish,waterbody,length,sensitive):
 		# number = pgselect * from fish_guide where waterbody_code=41508250 and specname=73 and length_category_id=40 and sensitive='Sensitive'
 		number = 8
 
-		fish = u'Coho Salmon'
-		waterbody = u'Lake Erie 1 - Western Basin'
-		length = u'40-45cm'
+		fish = u'Northern Pike'
+		waterbody = u'Hutchison Lake'
+		length = u'70-75cm'
 		sensitive = u'General'
 
 		conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 		cur = conn.cursor()
-		cur.execute("SELECT * FROM fish_guide_smol;")
-		db_test = str(cur.fetchone())
+		query_string = "SELECT * FROM fish_guide WHERE specname=\'" + fish + "\' AND guide_locname_eng=\'" + waterbody  +"\' AND population_type_desc=\'" + sensitive + "\' AND length_category_label=\'" + length + "\';"
+		print(query_string)
+		cur.execute(query_string)
+		number = str(int(cur.fetchone()[10]))
+
 		cur.close()
 		conn.close()
 
@@ -45,7 +48,7 @@ def db_query(fish,waterbody,length,sensitive):
 		text = text + "You can eat <strong>" + str(number) + "</strong> meals of this fish safely in one month. <br><br>"
 		text = text + "Add the details for another fish above and search again!"
 		#return "<p>" + " ".join([fish,waterbody,length,sensitive]) + "</p>"
-		return text + db_test
+		return text
 
 application=app
 if __name__ == '__main__':
